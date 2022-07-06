eafb6ea9f4ac401c4616907d4e7e4cbe86289607
Alberto Tudela <ajtudela@gmail.com>
2022-07-06 19:17:35 +0200

    First implementation of lifecycle node

--- include/sicks300_2/sicks300_2.hpp
+++ include/sicks300_2/sicks300_2.hpp
@@ -14,13 +14,16 @@
 
 // C++
 #include <string>
+#include <chrono>
 
 // BOOST
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/thread/thread.hpp>
 
 // ROS
-#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "rclcpp_lifecycle/lifecycle_publisher.hpp"
+#include "lifecycle_msgs/msg/transition.hpp"
 #include "std_msgs/msg/bool.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "diagnostic_msgs/msg/diagnostic_array.hpp"
@@ -28,23 +31,24 @@
 // Common
 #include "common/ScannerSickS300.h"
 
-class SickS3002: public rclcpp::Node{
-	public:
-		SickS3002(const std::string& name);
-		~SickS3002();
+using namespace std::chrono_literals;
 
-		bool open();
-		std::string getPort();
-		bool receiveScan();
-		void publishStandby(bool in_standby);
-		void publishLaserScan(std::vector<double> vdDistM, std::vector<double> vdAngRAD, std::vector<double> vdIntensAU, unsigned int iSickTimeStamp, unsigned int iSickNow);
-		void publishError(std::string error);
-		void publishWarn(std::string warn);
+typedef rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn rclcpp_CallReturn;
+
+class SickS3002: public rclcpp_lifecycle::LifecycleNode{
+	public:
+		SickS3002(const std::string& name, bool intra_process_comms = false);
+		~SickS3002();
+		rclcpp_CallReturn on_configure(const rclcpp_lifecycle::State &);
+		rclcpp_CallReturn on_activate(const rclcpp_lifecycle::State &);
+		rclcpp_CallReturn on_deactivate(const rclcpp_lifecycle::State &);
+		rclcpp_CallReturn on_cleanup(const rclcpp_lifecycle::State &);
+		rclcpp_CallReturn on_shutdown(const rclcpp_lifecycle::State & state);
 
 	private:
-		rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_pub_;
-		rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr in_standby_pub_;
-		rclcpp::Publisher<diagnostic_msgs::msg::DiagnosticArray>::SharedPtr diag_pub_;
+		rclcpp_lifecycle::LifecyclePublisher<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_pub_;
+		rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::Bool>::SharedPtr in_standby_pub_;
+		rclcpp_lifecycle::LifecyclePublisher<diagnostic_msgs::msg::DiagnosticArray>::SharedPtr diag_pub_;
 		std_msgs::msg::Bool in_standby_;
 		rclcpp::Time synced_ros_time_;
 
@@ -54,5 +58,16 @@ class SickS3002: public rclcpp::Node{
 		unsigned int synced_sick_stamp_;
 		double scan_duration_, scan_cycle_time_, communication_timeout_;
 		ScannerSickS300 scanner_;
+		rclcpp::TimerBase::SharedPtr timer_;
+
+		bool open();
+		std::string getPort();
+		bool receiveScan();
+		void publishStandby(bool in_standby);
+		void publishLaserScan(std::vector<double> vdDistM, std::vector<double> vdAngRAD, 
+							std::vector<double> vdIntensAU, unsigned int iSickTimeStamp, 
+							unsigned int iSickNow);
+		void publishError(std::string error);
+		void publishWarn(std::string warn);
 };
 #endif
